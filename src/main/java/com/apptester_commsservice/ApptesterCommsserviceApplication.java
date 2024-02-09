e57489dee1db7925e22274fd47d52a4f56840352
Jaklbela <KsZh11@mail.ru>
2024-02-09 21:12:24 +0300

    initial commit

---
+++ src/main/java/com/apptester_commsservice/ApptesterCommsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.apptester_commsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptesterCommsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptesterCommsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apptester_commsservice/ApptesterCommsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apptester_commsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptesterCommsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
